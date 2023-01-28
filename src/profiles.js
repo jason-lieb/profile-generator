60ea2679852b54d67ccc9dac5af3c4c31882d3b2
Jason Lieb <Jason.lieb@outlook.com>
2023-01-27 20:48:19 -0600

    finish styling

--- src/profiles.js
+++ src/profiles.js
@@ -8,8 +8,8 @@ function createProfileHTML(profiles, type) {
         icon = 'fa-solid fa-mug-hot';
         break;
       case 'Engineer':
-        lastProp = `Github: <a class="text-decoration-none" href="https://github.com/${profile.github}">${profile.github}</a>`;
-        icon = 'fa-regular fa-glasses';
+        lastProp = `Github: <a class="text-decoration-none" href="https://github.com/${profile.github}" target="_blank">${profile.github}</a>`;
+        icon = 'fa-solid fa-glasses';
         break;
       case 'Intern':
         lastProp = `School: ${profile.school}`;
@@ -20,10 +20,12 @@ function createProfileHTML(profiles, type) {
   <div class="card m-2 shadow">
     <div class="card-title bg-primary px-3 py-1 m-0">
       <h2 class="text-white mt-2">${profile.name}</h2>
-      <i class="${icon}"></i>
-      <h3 class="text-white">${type}</h3>
+      <div class="d-flex align-items-center mb-2">
+        <i class="${icon}" style="color: white;"></i>
+        <h3 class="text-white my-0 mx-2">${type}</h3>
+      </div>
     </div>
-    <div class="card-body bg-light">
+    <div class="card-body bg-light d-flex align-items-center">
       <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${profile.id}</li>
         <li class="list-group-item">Email: <a class="text-decoration-none" href="mailto:${profile.email}">${profile.email}</a></li>
@@ -46,7 +48,6 @@ function outputHTML(profileHTML) { //<link rel="stylesheet" href="./style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
   <title>Team Profile</title>
 </head>
 <body>
@@ -56,6 +57,7 @@ function outputHTML(profileHTML) { //<link rel="stylesheet" href="./style.css">
 <div class="container d-flex">
   ${profileHTML}
 </div>
+<script src="https://kit.fontawesome.com/63c059a934.js" crossorigin="anonymous"></script>
 </body>
 </html>
 `;
